39356a9e1d8f9d89c43b74abec353c17ce41d380
Ditto Pachen <dittopachen@yahoo.com>
2022-03-25 11:05:50 +0530

    Additional Details 1

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,11 @@ const forecast = (latitude, longitude, callback) => {
          } else if  (body.error){
              callback("unable to find the place",undefined)
          } else {
-            const data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out'
+            const data = body.current.weather_descriptions[0] 
+            + '. It is currently ' + body.current.temperature 
+            + ' degrees out. It feels like ' 
+            + body.current.feelslike + ' degrees out. The humidity is '
+            +body.current.humidity+ '%'
             callback(undefined, data)
          }
 })
